7379c877b9a2c12b83ac98dad8fa4b88c1df6cfa
monikrish2698 <65104881+monikrish2698@users.noreply.github.com>
2020-05-16 11:57:11 +0530

    Add files via upload

--- pro1_spider.py
+++ pro1_spider.py
@@ -1,15 +1,25 @@
 import scrapy
+from scrapy.http import FormRequest
+from scrapy.utils.response import open_in_browser
 from ..items import Project1Item
 
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
     start_urls = [
-        'http://quotes.toscrape.com/'
+        'http://quotes.toscrape.com/login'
     ]
 
     def parse(self, response):
+        token = response.css('form input::attr(value)').extract_first()
+        return FormRequest.from_response(response, formdata={
+            'csrf_token': token,
+            'username': 'Monish',
+            'password': 'uptownfunk'
+        }, callback=self.start_scraping)
 
+    def start_scraping(self, response):
+        open_in_browser(response)
         items = Project1Item()
 
         subtags = response.css('div.quote')
@@ -23,3 +33,8 @@ class QuotesSpider(scrapy.Spider):
             items['tags'] = tags
 
             yield items
+
+        next_page = response.css('li.next > a::attr(href)').get()
+
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.start_scraping)
